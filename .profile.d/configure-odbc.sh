287e2f4c7b79dfb5ec9583ba20d5dd079af8b503
Vladyslav Tymofeiev <vladyslavty@softwareplanetgroup.com>
2025-10-13 15:07:56 +0300

    Rollback script for update ODBC Driver configurations

--- .profile.d/configure-odbc.sh
+++ .profile.d/configure-odbc.sh
@@ -1,26 +1,36 @@
-##!/bin/bash
-#
-#for f in ${HOME}/.apt/opt/microsoft/*; do
-#	MS_ODBC_VERSION=$(echo "$(basename $f)" | grep -o -E '[0-9]+')
-#	break;
-#done
-#
-#for f in ${HOME}/.apt/usr/lib/*; do
-#	MS_ODBC_DRIVER_FILE=$(echo "$(basename $f)")
-#	break;
-#done
-#
-#export ODBCINI=${HOME}/.apt/usr/lib/odbc/conf/
-#
-#echo "[ODBC Driver $MS_ODBC_VERSION for SQL Server]
-#Description=Microsoft ODBC Driver $MS_ODBC_VERSION for SQL Server
-#Driver=/app/.apt/usr/lib/$MS_ODBC_DRIVER_FILE
-#UsageCount=1
-#" > ${ODBCINI}/odbc.ini
-#
-#export ODBCSYSINI=${HOME}/.apt/usr/lib/odbc/conf/
-#
-#echo "[ODBC Driver $MS_ODBC_VERSION for SQL Server]
-#Description=Microsoft ODBC Driver $MS_ODBC_VERSION for SQL Server
-#Driver=/app/.apt/usr/lib/$MS_ODBC_DRIVER_FILE
-#" > ${ODBCSYSINI}/odbcinst.ini
+#!/bin/bash
+set -e
+
+# Detect ODBC version
+for f in ${HOME}/.apt/opt/microsoft/*; do
+    MS_ODBC_VERSION=$(echo "$(basename "$f")" | grep -o -E '[0-9]+')
+    break
+done
+
+# Detect ODBC driver file (the .so file)
+for f in ${HOME}/.apt/usr/lib/*msodbcsql*.so; do
+    MS_ODBC_DRIVER_FILE=$(basename "$f")
+    break
+done
+
+# Ensure directories exist
+export ODBCINI=${HOME}/.apt/usr/lib/odbc/conf
+export ODBCSYSINI=${HOME}/.apt/usr/lib/odbc/conf
+mkdir -p "$ODBCINI"
+
+# Create ODBC config files
+cat <<EOF > ${ODBCINI}/odbc.ini
+[ODBC Driver ${MS_ODBC_VERSION} for SQL Server]
+Description=Microsoft ODBC Driver ${MS_ODBC_VERSION} for SQL Server
+Driver=/app/.apt/usr/lib/${MS_ODBC_DRIVER_FILE}
+UsageCount=1
+EOF
+
+cat <<EOF > ${ODBCSYSINI}/odbcinst.ini
+[ODBC Driver ${MS_ODBC_VERSION} for SQL Server]
+Description=Microsoft ODBC Driver ${MS_ODBC_VERSION} for SQL Server
+Driver=/app/.apt/usr/lib/${MS_ODBC_DRIVER_FILE}
+EOF
+
+echo "ODBC configuration generated:"
+cat ${ODBCINI}/odbc.ini
--- lib/phpsql.sh
+++ lib/phpsql.sh
@@ -42,24 +42,24 @@ indent() {
 BUILD_DIR=$1
 BP_DIR=$2
 
-## Get microsoft ODBC driver version
-#for f in ${BUILD_DIR}/.apt/opt/microsoft/*; do
-#	MS_ODBC_VERSION=$(echo "$(basename $f)" | grep -o -E '[0-9]+')
-#	break;
-#done
-#
-#arrow "Starting adding ODBC Driver ${MS_ODBC_VERSION} for SQL Server"
-#mkdir -p "${BUILD_DIR}/.apt/usr/lib/odbc/conf/" | indent
-#mkdir -p "${BUILD_DIR}/.apt/usr/share/resources/en_US/" | indent
-#cp -a "${BUILD_DIR}/.apt/opt/microsoft/msodbcsql${MS_ODBC_VERSION}/lib64/." "${BUILD_DIR}/.apt/usr/lib/" | indent
-#arrow "copied libmsodbcsql-${MS_ODBC_VERSION}-*"
-#cp -a "${BUILD_DIR}/.apt/opt/microsoft/msodbcsql${MS_ODBC_VERSION}/share/resources/en_US/." "${BUILD_DIR}/.apt/usr/share/resources/en_US/" | indent
-#arrow "copied msodbcsqlr${MS_ODBC_VERSION}.rll"
-#
-#mkdir -p "${BUILD_DIR}/.profile.d" | indent
-#cp "$BP_DIR/.profile.d/configure-odbc.sh" "${BUILD_DIR}/.profile.d" | indent
-#arrow "copied profile.d"
-#arrow "Finished adding ODBC Driver ${MS_ODBC_VERSION} for SQL Server"
+# Get microsoft ODBC driver version
+for f in ${BUILD_DIR}/.apt/opt/microsoft/*; do
+	MS_ODBC_VERSION=$(echo "$(basename $f)" | grep -o -E '[0-9]+')
+	break;
+done
+
+arrow "Starting adding ODBC Driver ${MS_ODBC_VERSION} for SQL Server"
+mkdir -p "${BUILD_DIR}/.apt/usr/lib/odbc/conf/" | indent
+mkdir -p "${BUILD_DIR}/.apt/usr/share/resources/en_US/" | indent
+cp -a "${BUILD_DIR}/.apt/opt/microsoft/msodbcsql${MS_ODBC_VERSION}/lib64/." "${BUILD_DIR}/.apt/usr/lib/" | indent
+arrow "copied libmsodbcsql-${MS_ODBC_VERSION}-*"
+cp -a "${BUILD_DIR}/.apt/opt/microsoft/msodbcsql${MS_ODBC_VERSION}/share/resources/en_US/." "${BUILD_DIR}/.apt/usr/share/resources/en_US/" | indent
+arrow "copied msodbcsqlr${MS_ODBC_VERSION}.rll"
+
+mkdir -p "${BUILD_DIR}/.profile.d" | indent
+cp "$BP_DIR/.profile.d/configure-odbc.sh" "${BUILD_DIR}/.profile.d" | indent
+arrow "copied profile.d"
+arrow "Finished adding ODBC Driver ${MS_ODBC_VERSION} for SQL Server"
 
 # Cleanup
 rm -f sqlsrv.tar.gz 2>/dev/null || true
